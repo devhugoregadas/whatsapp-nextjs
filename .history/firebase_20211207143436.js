4535f24728cf092734486ec816e21f04e6124b03
Hugo <dev.hugoregadas@gmail.com>
2021-12-07 14:37:36 +0100

    firebase API

--- .history/firebase_20211207143436.js
+++ .history/firebase_20211207143436.js
@@ -1,12 +1,23 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyB0exmkcH_L9yfUK-1rxe9nL6JGrv5O6v8",
   authDomain: "whatsapp-nextjs-a298a.firebaseapp.com",
   projectId: "whatsapp-nextjs-a298a",
   storageBucket: "whatsapp-nextjs-a298a.appspot.com",
   messagingSenderId: "912029637464",
   appId: "1:912029637464:web:feb0f3500811b758d1e32d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 };
 
 const app = !firebase.apps.length
--- .history/firebase_20211207143443.js
+++ .history/firebase_20211207143443.js
@@ -1,12 +1,9 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0exmkcH_L9yfUK-1rxe9nL6JGrv5O6v8",
-  authDomain: "whatsapp-nextjs-a298a.firebaseapp.com",
-  projectId: "whatsapp-nextjs-a298a",
-  storageBucket: "whatsapp-nextjs-a298a.appspot.com",
-  messagingSenderId: "912029637464",
-  appId: "1:912029637464:web:feb0f3500811b758d1e32d"
+  /**
+   * PASTE FIREBASE API CREDENTIALS HERE
+   * */
 };
 
 const app = !firebase.apps.length
